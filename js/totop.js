1c349a729e6b482ba19b72ebb3d5aeeb69b75ce3
IMLCB <1491688839@qq.com>
2015-08-20 23:27:28 +0800

    Site updated: 2015-08-20 23:27:27

--- js/totop.js
+++ js/totop.js
@@ -1,7 +1,7 @@
 (function($) {
     // When to show the scroll link
     // higher number = scroll link appears further down the page
-    var upperLimit = 1000;
+    var upperLimit = 300;
 
     // Our scroll link element
     var scrollElem = $('#totop');
