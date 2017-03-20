7015e40160f81a9c01a09c801ea08cf1ca84b3cd
lichengbo <chengboli01@gmail.com>
2017-03-20 10:52:19 +0800

    Site updated: 2017-03-20 10:52:11

---
+++ demo/vue-note-app/src/components/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="note-editor">
+    <textarea class="form-control" @input="edit(activeNote)">{{activeNote.text}}</textarea>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            activeNote: Object
+        },
+        methods: {
+            edit(activeNote) {
+                this.$emit("edit", activeNote, event);
+            }
+        }
+    }
+</script>
